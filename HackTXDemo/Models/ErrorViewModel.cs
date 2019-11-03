5e79e22643474ef9ba6ba868fdbd8bc64af95583
Shawn Weisfeld <sweisfel@microsoft.com>
2019-11-02 22:10:32 -0500

    Add project files.

---
+++ HackTXDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HackTXDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
